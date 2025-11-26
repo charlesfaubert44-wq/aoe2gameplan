e39184efa6e6b5517ffb663a91d2dbacb765900e
charlesfaubert44-wq <charlesfaubert44@gmail.com>
2025-11-26 13:25:28 -0700

    fix: update PrismaAdapter import to use @auth/prisma-adapter

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import { NextAuthOptions } from 'next-auth'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '@/lib/prisma'
 
 // Type definitions
